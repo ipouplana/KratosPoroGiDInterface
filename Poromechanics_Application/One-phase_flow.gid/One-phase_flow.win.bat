e452668c5308056865f90a15b1c40da6474d6c2e
xaviertort <xavitort1@gmail.com>
2024-05-09 09:26:11 +0200

    Create branch

--- Poromechanics_Application/One-phase_flow.gid/One-phase_flow.win.bat
+++ Poromechanics_Application/One-phase_flow.gid/One-phase_flow.win.bat
@@ -8,9 +8,10 @@ rem currentdirectory = %2
 rem problemtypedirectory = %3
 
 rem Setting paths. WARNING: one should check them before running this file
-set PATH="C:\\Users\\Xavi Tort\\source\\repos\\KratosMultiphysics\\Kratos\\bin\\FullDebug\\libs";%PATH%
-set PYTHONPATH="C:\\Users\\Xavi Tort\\source\\repos\\KratosMultiphysics\\Kratos\\bin\\FullDebug";%PYTHONPATH%
+set PATH="C:\\Users\\xavit\\Repositoris\\Kratos\\bin\\FullDebug\\libs";%PATH%
+set PYTHONPATH="C:\\Users\\xavit\\Repositoris\\Kratos\\bin\\FullDebug";%PYTHONPATH%
 
 
 rem Execute the program
-"C:\\Users\\Xavi Tort\\AppData\\Local\\Programs\\Python\Python39\\python.exe" MainKratos.py > %1.info 2> %1.err
+"C:\\Users\\xavit\\AppData\\Local\\Programs\\Python\\Python39\\python.exe" MainKratos.py > %1.info 2> %1.err
+
